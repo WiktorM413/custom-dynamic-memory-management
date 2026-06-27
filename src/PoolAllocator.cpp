5254a74ed912053264a5066968eef020b1626450
WiktorM413 <wiktor.markowski81@gmail.com>
2026-06-27 11:49:01 +0200

    Implement PoolAllocator::AllocateBlock method

--- src/PoolAllocator.cpp
+++ src/PoolAllocator.cpp
@@ -1,3 +1,23 @@
 #include "../include/PoolAllocator.h"
+#include <cstdint>
 
 PoolAllocator::PoolAllocator(std::size_t chunksPerBlock): chunksPerBlock(chunksPerBlock), allocator(nullptr) {}
+
+PoolAllocator::Chunk* PoolAllocator::AllocateBlock(const std::size_t& chunkSize)
+{
+	const std::size_t blockSize = this->chunksPerBlock * chunkSize;
+
+	Chunk* begin = reinterpret_cast<Chunk*>(blockSize);
+
+	Chunk* curr = begin;
+	for (std::size_t i = 0; i < this->chunksPerBlock - 1; i++)
+	{
+		curr->next = reinterpret_cast<Chunk*>(reinterpret_cast<uint8_t*>(curr) + chunkSize);
+
+		curr = curr->next;
+	}
+
+	curr->next = nullptr;
+
+	return begin;
+}
