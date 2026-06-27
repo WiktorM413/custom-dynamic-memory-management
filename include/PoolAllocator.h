0be91340fb780a173f476eaa7803b9bdc374f9f1
WiktorM413 <wiktor.markowski81@gmail.com>
2026-06-27 12:30:00 +0200

    Add class template to PoolAllocator, implement destructor, fix AllocateBlock method

--- include/PoolAllocator.h
+++ include/PoolAllocator.h
@@ -1,18 +1,41 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+template<typename T>
 class PoolAllocator
 {
 public:
-	PoolAllocator(std::size_t chunksPerBlock);
-
-	template<typename T>
-	T* Allocate()
+	PoolAllocator(std::size_t chunksPerBlock): chunksPerBlock(chunksPerBlock), allocator(nullptr), blocks({}) {}
+	~PoolAllocator()
 	{
-		
+		for (int i = 0; i < this->blocks.size(); i++)
+		{
+			free(blocks[i]);
+		}
 	}
 
-	template<typename T>
+	T* Allocate()
+	{
+		if (! this->allocator)
+		{
+			this->allocator = this->AllocateBlock(sizeof(T));
+
+			this->blocks.push_back(this->allocator);
+		}
+		
+		Chunk* freeChunk = this->allocator;
+		
+		this->allocator = this->allocator->next;
+		
+
+		return reinterpret_cast<T*>(freeChunk);
+	}
+
 	void Deallocate(T* ptr)
 	{
 		
@@ -24,8 +47,28 @@ private:
 		Chunk* next;
 	};
 
-	Chunk* AllocateBlock(const std::size_t& chunkSize);
+	Chunk* AllocateBlock(std::size_t chunkSize)
+	{
+		chunkSize = std::max(chunkSize, sizeof(Chunk));
+		
+		const std::size_t blockSize = this->chunksPerBlock * chunkSize;
+
+		Chunk* begin = reinterpret_cast<Chunk*>(malloc(blockSize));
+
+		Chunk* curr = begin;
+		for (std::size_t i = 0; i < this->chunksPerBlock - 1; i++)
+		{
+			curr->next = reinterpret_cast<Chunk*>(reinterpret_cast<uint8_t*>(curr) + chunkSize);
+
+			curr = curr->next;
+		}
+
+		curr->next = nullptr;
+
+		return begin;
+	}
 	
-	std::size_t chunksPerBlock;
-	Chunk*      allocator;
+	std::size_t              chunksPerBlock;
+	Chunk*                   allocator;
+	std::vector<Chunk*>      blocks;
 };
