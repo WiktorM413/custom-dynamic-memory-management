375942b60d03b506e62057fe2138e8c0c13c18aa
usern-4me <mazurkiewicz.leon@gmail.com>
2026-07-05 19:14:05 +0200

    cleaned main

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,8 @@
-#include "../include/LinearAllocator.h"
-#include <exception>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Starting allocator test...\n";
-
-	try {
-		FrameAllocator allocator(sizeof(int) * 1000000, 64);
-
-		TightAllocator allocator2(sizeof(int) * 1000000, 16);
-
-	} catch (const std::exception& e) {
-		std::cout << "Exception caught: " << e.what() << "\n";
-	}
+	std::cout << "Hello World!" << std::endl;
 
 	return 0;
 }
